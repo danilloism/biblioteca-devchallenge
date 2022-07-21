63c45b933fa6502da05b4940ea0e6ebb188cf9b2
Danillo Ilggner <danilloilggner@gmail.com>
2022-07-21 19:36:12 -0300

    refactor: pequenas mudanças

--- apps/fastify/src/main.ts
+++ apps/fastify/src/main.ts
@@ -1,10 +1,21 @@
+import 'dotenv/config';
 import Fastify from 'fastify';
 import autoLoad from '@fastify/autoload';
 import { join } from 'path';
+import compress from '@fastify/compress';
+import pino from 'pino';
+import cors from '@fastify/cors';
+import helmet from '@fastify/helmet';
 
 const start = async () => {
-  const server = Fastify({ logger: true });
+  const env = process.env.ENV;
+  const server = Fastify({
+    logger: pino({ level: env == 'prod' ? 'info' : 'trace' }),
+  });
 
+  server.register(compress);
+  server.register(helmet);
+  server.register(cors);
   server.register(autoLoad, { dir: join(__dirname, 'plugins') });
   server.register(autoLoad, { dir: join(__dirname, 'rotas') });
 
